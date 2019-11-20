849d6ccd6fb8871617f490979094b356e1cfda4c
BurlakovEP <burlakov_e_p@mail.ru>
2019-11-20 11:28:48 +0600

    Updates

---
+++ precache-manifest.a2416fbfa2e1515124f0a9f027031f9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8c0096a4855680589c582dbdbb3f1d",
+    "url": "/Money-transfer/index.html"
+  },
+  {
+    "revision": "ddabd4db242560d14ffb",
+    "url": "/Money-transfer/static/css/main.94d57e04.chunk.css"
+  },
+  {
+    "revision": "1cdb1581f8eea9f1d194",
+    "url": "/Money-transfer/static/js/2.5af7a278.chunk.js"
+  },
+  {
+    "revision": "ddabd4db242560d14ffb",
+    "url": "/Money-transfer/static/js/main.ac576d64.chunk.js"
+  },
+  {
+    "revision": "3d52c057c335848748a5",
+    "url": "/Money-transfer/static/js/runtime-main.1819ed8a.js"
+  }
+]);
